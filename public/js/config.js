1c642e581b058d44cd0be5ac7f98c4995ba0b11f
Ngô Hồng Thái <36537627+ngohongthai1402@users.noreply.github.com>
2022-03-19 16:37:08 +0700

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,49 @@
+const WEB = {
+  title: "PAYMENT",
+  favicon: "favicon.png",
+};
+
+const USER = {
+  username: "Ngô Hồng Thái ",
+  blueTick: false,
+  avatar: "logonhan.png",
+  skills: [
+      "Social Media",
+      "Chế Ảnh"
+  ],
+  banks: [
+      {
+          name: "Vietcombank",
+          logo: "vcb.jpg",
+          number: "1025002449",
+          owner: "NGO HONG THAI"
+      },
+      {
+          name: "MB Bank ",
+          logo: "mb.png",
+          number: "1234561997",
+          owner: "NGO HONG "
+      },
+      {
+         name: "Momo",
+         logo: "momo.jpg",
+         number: "0349333397",
+         owner: "NGO HONG THAI",
+         fastTransfer: true
+    },
+      {
+          name: "Thẻ Siêu Rẻ",
+          logo: "div_.png",
+          number: "ngohongthai",
+          owner: "NGO HONG THAI"
+      },
+      {
+          name: "Thẻ Cào Siêu Rẻ",
+          logo: "cc.png",
+          number: "ngohongthai",
+          owner: "NGO HONG THAI"
+      },
+
+
+  ]
+};
